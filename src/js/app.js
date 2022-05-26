46a99692ff0327c322727d8a49b92628fac68cd7
Pasidnu <pasindu@marlincommunications.com>
2022-05-26 14:41:25 +0530

    Feedback 1, Mobile menu btn fixed, animation added

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
 import "bootstrap/js/dist/collapse";
+import AOS from "aos"
+
+// const AOS = require('aos')
+
+AOS.init();
+
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    var link = document.querySelector('.navbar-toggler');
+    link.addEventListener('click', function() {
+      if (link.classList.contains('toggle-menu--clicked')) {
+        link.classList.remove('toggle-menu--clicked');
+      } else {
+        link.classList.add('toggle-menu--clicked');
+      }
+    }, false);
+  }, false);
